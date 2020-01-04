56d938b8e4220f40f84bab62a23a2cb3eee8020b
aakashverma1124 <info.aakash11@gmail.com>
2020-01-04 14:12:34 +0530

    Topic Resource Completed

---
+++ src/main/java/in/aboutaakash/springbootstarter/topic/TopicService.java
@@ -0,0 +1,46 @@
+package in.aboutaakash.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("laravel", "Laravel Framework", "Laravel Framework Description"),
+			new Topic("django", "Django Framework", "Django Framework Description")
+			));
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();		
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+	
+	public void updateTopic(String id, Topic topic) {
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+		
+	}
+}
+
